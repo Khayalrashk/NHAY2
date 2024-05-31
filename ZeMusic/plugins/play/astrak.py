bfd74e055e4625dafa1189bf0f2708887b64a646
fasdxoox <163231340+fasdxoox@users.noreply.github.com>
2024-06-01 02:27:23 +0300

    Update astrak.py

--- ZeMusic/plugins/play/astrak.py
+++ ZeMusic/plugins/play/astrak.py
@@ -6,15 +6,22 @@ from pyrogram.errors import UserNotParticipant
 from ZeMusic import app
 
 channel = "eo_u7"
+command_list = [
+    "تشغيل", "بحث", "تخطي", "استئناف", "تقديم", "تحميل", "توقف", "مؤقت", "كمل", "كملي", 
+    "لارين بحث", "غنيلي", "شعر", "قران", "اذكار", "ادعيه", "play", "شغلي", "شغل", "vplay", 
+    "vتشغيل", "cplay", "cvplay", "playforce", "vplayforce", "cplayforce", "cvplayforce", 
+    "start", "stats", "الاوامر", "اوامر", "ميوزك", "بنج", "سرعه", "song", "/song", "/start"
+]
+
 async def subscription(_, __: Client, message: Message):
     user_id = message.from_user.id
     try: await app.get_chat_member(channel, user_id)
     except UserNotParticipant: return False
     return True
-    
+
 subscribed = filters.create(subscription)
 
-@app.on_message(~subscribed & filters.command(["تشغيل", "بحث", "تخطي", "استئناف", "تقديم", "تحميل", "توقف", "مؤقت", "كمل", "كملي", "لارين بحث", "غنيلي", "شعر", "قران", "اذكار", "ادعيه", "play", "شغلي", "شغل", "vplay", "vتشغيل", "cplay", "cvplay", "playforce", "vplayforce", "cplayforce", "cvplayforce", "start", "stats", "الاوامر", "اوامر", "ميوزك", "بنج", "سرعه", "song", "/song", "/start"]))
+@app.on_message(~subscribed & filters.command(command_list))
 async def checker(_: Client, message: Message):
     if message.chat.type in [ChatType.GROUP, ChatType.SUPERGROUP]: await message.delete()
     user_id = message.from_user.id
