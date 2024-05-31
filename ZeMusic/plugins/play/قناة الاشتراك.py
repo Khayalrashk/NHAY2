6c0b8c228b5eb72503b37e280ad1ed84e5278e7b
fasdxoox <163231340+fasdxoox@users.noreply.github.com>
2024-06-01 01:30:54 +0300

    Update قناة الاشتراك.py

--- ZeMusic/plugins/play/قناة الاشتراك.py
+++ ZeMusic/plugins/play/قناة الاشتراك.py
@@ -3,6 +3,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from ZeMusic import app
 from config import Muntazer, OWNER_ID
 
+Muntazer = "eo_u7"
 @app.on_message(filters.private & filters.user(OWNER_ID))
 async def must_join_channel(_, message):
     if "‹ قناة الاشتراك ›" in message.text:
