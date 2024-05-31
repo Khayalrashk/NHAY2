66aac4700f404e220e7cb78d8d74becc861e4aba
fasdxoox <163231340+fasdxoox@users.noreply.github.com>
2024-05-31 23:26:46 +0300

    Update config.py

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Khayalrashk/NHAY",
+    "https://github.com/Khayalrashk/NHAY2",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
